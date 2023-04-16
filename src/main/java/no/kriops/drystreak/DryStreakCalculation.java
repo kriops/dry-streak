6eaf9b5ffbe17736e5b309112392793f8ec52fd8
iProdigy <gitProdigy@proton.me>
2023-04-16 16:05:13 -0700

    refactor: remove vavr dependence

--- src/main/java/no/kriops/drystreak/DryStreakCalculation.java
+++ src/main/java/no/kriops/drystreak/DryStreakCalculation.java
@@ -1,11 +1,11 @@
 package no.kriops.drystreak;
 
-import io.vavr.control.Try;
 import net.runelite.client.ui.components.FlatTextField;
 import net.runelite.client.ui.components.shadowlabel.JShadowedLabel;
 
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.util.Optional;
 
 import org.apache.commons.lang3.math.Fraction;
 import org.apache.commons.math3.distribution.GeometricDistribution;
@@ -22,22 +22,40 @@ public class DryStreakCalculation implements ActionListener {
     @Override
     public void actionPerformed(ActionEvent e) {
         final String explanation = "At this number of kills, you are less likely to have received zero drops than not.";
-        final String input = inputField.getText();
-        Try
-                .of(() -> 1.0 / Integer.parseInt(input))
-                .orElse(Try.of(() -> Double.parseDouble(input)))
-                .orElse(Try.of(() -> Fraction.getFraction(input).doubleValue()))
-                .toEither("Unable to parse input.")
-                .filterOrElse(p -> 0 < p && p <= 1, integer -> "Drop rate must be greater than zero.")
-                .flatMap(p -> Try.of(() -> new GeometricDistribution(null, p))
-                        .mapTry(dist -> dist.inverseCumulativeProbability(0.5))
-                        .toEither("Error performing calculation."))
-                .map(result -> String.format("<p>Expected dry streak: %s</p><br><p>%s</p>", result, explanation))
-                .map(this::asHtml)
-                .peek(outputField::setText)
-                .mapLeft(this::asHtml)
-                .peekLeft(outputField::setText)
-        ;
+        String message = Optional.ofNullable(inputField.getText())
+            .map(input -> {
+                try {
+                    return 1.0 / Integer.parseInt(input);
+                } catch (NumberFormatException ignored) {
+                }
+
+                try {
+                    return Double.parseDouble(input);
+                } catch (NumberFormatException ignored) {
+                }
+
+                try {
+                    return Fraction.getFraction(input).doubleValue();
+                } catch (NumberFormatException ignored) {
+                }
+
+                return null;
+            })
+            .map(p -> {
+                if (0 < p && p <= 1) {
+                    try {
+                        int result = new GeometricDistribution(null, p).inverseCumulativeProbability(0.5);
+                        return String.format("<p>Expected dry streak: %s</p><br><p>%s</p>", result, explanation);
+                    } catch (Exception ignored) {
+                        return "Error performing calculation.";
+                    }
+                } else {
+                    return "Drop rate must be greater than zero.";
+                }
+            })
+            .orElse("Unable to parse input.");
+
+        outputField.setText(asHtml(message));
     }
 
     private String asHtml(String input) {
